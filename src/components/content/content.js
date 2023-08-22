44b34856da83978e56af40c66e571eec25f5bfec
SlowNibbler <ht8814@gmail.com>
2023-08-21 20:49:44 -0700

    dir updates

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -6,8 +6,8 @@ import {
   NavLink,
   HashRouter
 } from "react-router-dom";
-import ArtContainer from './routes/Art/artPage'
-import Home from './routes/Home/homePage'
+import ArtContainer from './artPage/artPage'
+import Home from './homePage/homePage'
 
 import Sidebar from '../sidebar/sidebar'
 
@@ -21,11 +21,44 @@ class Content extends Component{
             <Route className="homePath" exact path="/" component={Home}/>
             <Route path="/ArtContainer" component={ArtContainer}/>
           </Routes>
-        </HashRouter>      
+        </HashRouter>
+
       </div>
     );
   }
 }
+
+// function Layout() {
+//   return (
+//     <div>
+//       {/* A "layout route" is a good place to put markup you want to
+//           share across all the pages on your site, like navigation. */}
+//       <nav>
+//         <ul>
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/about">About</Link>
+//           </li>
+//           <li>
+//             <Link to="/dashboard">Dashboard</Link>
+//           </li>
+//           <li>
+//             <Link to="/nothing-here">Nothing Here</Link>
+//           </li>
+//         </ul>
+//       </nav>
+
+//       <hr />
+
+//       {/* An <Outlet> renders whatever child route is currently active,
+//           so you can think about this <Outlet> as a placeholder for
+//           the child routes we defined above. */}
+//       <Outlet />
+//     </div>
+//   );
+// }
 
 
 
