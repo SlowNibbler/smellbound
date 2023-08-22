d892386a273d897ab03e650eaa313cdb404734ea
SlowNibbler <ht8814@gmail.com>
2023-08-21 20:50:24 -0700

    dir updates

---
+++ src/components/content/artPage/artPage.js
@@ -0,0 +1,21 @@
+import { Component } from "react";
+import {
+  Routes,
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+
+class ArtContainer extends Component{
+  render() {
+    return (
+      <div className="art">
+           
+      </div>
+    );
+  }
+}
+
+
+
+export default ArtContainer;
---
+++ src/components/content/homePage/homePage.js
@@ -0,0 +1,21 @@
+import { Component } from "react";
+import {
+  Routes,
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+
+class HomePage extends Component{
+  render() {
+    return (
+      <div className="home">
+           
+      </div>
+    );
+  }
+}
+
+
+
+export default HomePage;
